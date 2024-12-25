44b7585075ebe5fe1a85722b84b9a1075d3a364e
udejej <gabriel4545@outlook.fr>
2024-12-25 12:04:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -21,13 +21,13 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["915321015515643935", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   musicardTheme:"themes16", //Goes from themes1 to themes19
-  activityName: "You", // This is bot status Write Anything here 
-  activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
+  activityName: "Lille RP", // This is bot status Write Anything here 
+  activityType: "PLAYING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   CheckmarkIcon: "https://cdn.discordapp.com/emojis/819446784647757834.gif",
   MusicIcon:"https://cdn.discordapp.com/emojis/763415718271385610.gif",
